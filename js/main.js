ae86fabef6b78cf8a7cbfd1944f0af8cae3c59d4
KniffleProject <hauke.hecht@gmail.com>
2018-04-07 16:30:57 +0200

    runden fertig

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
+var diceRoll = 0;
+
+
 function rollDice() {
+	if(diceRoll < 3){
   	var max = 6;
     var min = 1;
   	var dices = document.getElementsByClassName("demo");
-  	
+  	var buttonDiceRoll =  document.getElementById("diceRoll");
+	
+	buttonDiceRoll.innerHTML = "Roll Dice ("+(diceRoll+1)+"/3)";
+	diceRoll++;
+	//alert(diceRoll);
   	for(var i=0; i<dices.length;i++){
   	if(dices[i].myVar != "f"){
       var x = Math.round(Math.random() * (max - min)) + min;
     dices[i].innerHTML=x;
     }
   }  
+	}
 }
   
 function changeButton(btn){
@@ -21,10 +30,18 @@ function changeButton(btn){
 	};
 }
   
-  
-function rollDice2() {
-	var max = 6;
-	var min = 1;
-	var x = Math.round(Math.random() * (max - min)) + min;
-	document.getElementById("demo").innerHTML = x;
+function newRound(){
+	var buttonDiceRoll =  document.getElementById("diceRoll");
+	diceRoll = 0; 
+	buttonDiceRoll.innerHTML = "Roll Dice (0/3)";
+	resetDices()
+}
+
+function resetDices(){
+	var dices = document.getElementsByClassName("demo");
+	for(var i=0; i<dices.length;i++){
+		dices[i].style.backgroundColor="gray";
+		dices[i].innerHTML="D";
+		dices[i].myVar = "egal";
+	}
 }
