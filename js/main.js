af7ce1701e3e67d2ac66c1bed02bb980082384a5
Florian Beese <beese@fh-stralsund.de>
2018-04-07 16:02:32 +0200

    fixed layout integration

--- js/main.js
+++ js/main.js
@@ -4,28 +4,27 @@ function rollDice() {
   	var dices = document.getElementsByClassName("demo");
   	
   	for(var i=0; i<dices.length;i++){
-  	if(dices[i].id=="t"){
+  	if(dices[i].myVar != "f"){
       var x = Math.round(Math.random() * (max - min)) + min;
     dices[i].innerHTML=x;
     }
   }  
 }
   
-  function chngBttn(btn){
-  if(btn.id=="t"){
-    btn.setAttribute("id","f");
-  	btn.style.backgroundColor="#FFFFFF";
-  }else{
-    btn.setAttribute("id","t");
-    btn.style.backgroundColor="gray";
-  };
-    
-  }
+function changeButton(btn){
+	if(btn.myVar != "f"){
+		btn.myVar ="f";
+		btn.style.backgroundColor="#FFFFFF";
+	} else {
+		btn.myVar = "egal";
+		btn.style.backgroundColor="gray";
+	};
+}
   
   
-  function rollDice2() {
-  	var max = 6;
-    var min = 1;
-  	var x = Math.round(Math.random() * (max - min)) + min;
-  	document.getElementById("demo").innerHTML = x;
-    }
+function rollDice2() {
+	var max = 6;
+	var min = 1;
+	var x = Math.round(Math.random() * (max - min)) + min;
+	document.getElementById("demo").innerHTML = x;
+}
